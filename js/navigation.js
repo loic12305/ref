4372adf8634649c7a7ab459275756c9c202651af
Loïc <>
2013-05-13 12:45:52 +0000

    traduction "fr" du site et "en" des sources

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,11 @@
-function decache(contenu)
+function display_content(content)
 {
     document.getElementById('contenu_home').style.display='none';
     document.getElementById('contenu_story').style.display='none';
     document.getElementById('contenu_gallery').style.display='none';
     document.getElementById('contenu_contact').style.display='none';
 
-    new Effect.Appear(document.getElementById(contenu));
+    new Effect.Appear(document.getElementById(content));
     
     
 }
