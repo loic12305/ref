3db507a303d6c9b130d18d6919594777f2af5d7d
Loïc <>
2013-05-13 09:39:35 +0000

    optimisation du formulaire de contact + configuration avec le webmail des rillettes avec gmail

--- trait_form.php
+++ trait_form.php
@@ -1,21 +1,123 @@
 <?php
-$prenom = $_POST['prenom']; 
-$nom = $_POST['nom']; 
-$phone = $_POST['phone'];
-$mail = $_POST['mail'];
-$texte = $_POST['texte'];
+    ob_start();
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
+$destinataire = 'admin@lesrillettesenfut.fr';
+ 
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui'; // 'oui' ou 'non'
+ 
+// Messages de confirmation du mail
+$message_envoye = "Votre message nous est bien parvenu !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+ 
+// Messages d'erreur du formulaire
+$message_erreur_formulaire = "Vous devez d'abord <a href=\"contact.html\">envoyer le formulaire</a>.";
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+ 
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+ 
+// on teste si le formulaire a été soumis
+if (!isset($_POST['envoi']))
+{
+	// formulaire non envoyé
+	echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+}
+else
+{
+	/*
+	 * cette fonction sert à nettoyer et enregistrer un texte
+	 */
+	function Rec($text)
+	{
+		$text = htmlspecialchars(trim($text), ENT_QUOTES);
+		if (1 === get_magic_quotes_gpc())
+		{
+			$text = stripslashes($text);
+		}
+ 
+		$text = nl2br($text);
+		return $text;
+	};
+ 
+	/*
+	 * Cette fonction sert à vérifier la syntaxe d'un email
+	 */
+	function IsEmail($email)
+	{
+		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+		return (($value === 0) || ($value === false)) ? false : true;
+	}
+ 
+	// formulaire envoyé, on récupère tous les champs.
+	$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+	$prenom     = (isset($_POST['prenom']))     ? Rec($_POST['prenom'])     : '';
+	$tel     = $_POST['phone'];
+	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+	$objet   = "Demande du site de la fanfare";
+	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+	$message = $message."
+email : ".$email." tel : ".$tel;
+
+	$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
+
+	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
+	{
+		// les 4 variables sont remplies, on génère puis envoie le mail
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'From:'.$nom.' '.$prenom.' <'.$email.'>' . "\r\n" .
+					'Reply-To:'.$email. "\r\n" .
+					'X-Mailer:PHP/'.phpversion();
+ 
+		// envoyer une copie au visiteur ?
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.','.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+ 
+		// Remplacement de certains caractères spéciaux
+		$message = str_replace("&#039;","'",$message);
+		$message = str_replace("&#8217;","'",$message);
+		$message = str_replace("&quot;",'"',$message);
+		$message = str_replace('<br>','',$message);
+		$message = str_replace('<br />','',$message);
+		$message = str_replace("&lt;","<",$message);
+		$message = str_replace("&gt;",">",$message);
+		$message = str_replace("&amp;","&",$message);
+ 
+		// Envoi du mail
+		if (mail($cible, $objet, $message, $headers))
+		{
+			echo '<p>'.$message_envoye.'</p>'."\n";
+			header ("Refresh: 2;URL=index.html");
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>'."\n";
+			header ("Refresh: 2;URL=index.html");
+			
+		};
+	}
+	else
+	{
+		// une des 3 variables (ou plus) est vide ...
+		echo '<p>'.$message_formulaire_invalide.' <a href="contact.html">Retour au formulaire</a></p>'."\n";
+	};
+}; // fin du if (!isset($_POST['envoi']))
 
 
-$message= "Message de : $prenom $nom - tel : $phone mail : $mail <br/> $texte";
 
-
-     if(mail('loic.entressangle@gmail.com', 'Contact site Rillettes en Fût', $message)) 
-     { 
-          echo 'Le message a bien été envoyé'; 
-     } 
-     else 
-     { 
-          echo 'Le message n\'a pu être envoyé'; 
-     } 
 
 ?>
