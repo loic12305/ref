43551a40a6a24050078fa4ae1bbe3a06fca5be31
Loïc <>
2013-06-03 19:57:30 +0000

    > ajout meta normal pour le referencement  > ajout de la rubrique video (+3video)

--- js/navigation.js
+++ js/navigation.js
@@ -4,6 +4,7 @@ function display_content(content)
     document.getElementById('contenu_story').style.display='none';
     document.getElementById('contenu_gallery').style.display='none';
     document.getElementById('contenu_contact').style.display='none';
+    document.getElementById('contenu_video').style.display='none';
 
     new Effect.Appear(document.getElementById(content));
     
