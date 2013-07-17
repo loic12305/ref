405f0bf3e7e56bd96425bb90ed8a29f54903847b
Loïc <>
2013-07-17 09:43:15 +0000

    #--- 1.3.1 ---# > modif du clic sur la photo : ouvre les photos en taille originale     - création de la fonction "centerElmt" qui centre suivant la pop de news ou d'images (div toute la page, image centrer dans div)     - recalcule les dimensions ... > modif mention en "Survolez (aperçu) ou cliquez (taille originale) sur les photos" > ajout repertoire "images/gallery/HD"

--- js/popover.js
+++ js/popover.js
@@ -2,23 +2,42 @@
 // affiche de maniere animee une popOver  //
 ////////////////////////////////////////////
 function showPopOver(divID) {
-    //on le centre
-    document.getElementById(divID).style.left = "50%";
-    document.getElementById(divID).style.top = "50%";
     // on l'affiche
+    //-- rend invisible
     document.getElementById(divID).style.opacity='0';
-    document.getElementById(divID).style.display = "block";
+    document.getElementById(divID).style.display = "block";  
+
+    //on centre
+    centerElmt(divID);
+    // rend opac en anime
     $('#'+divID).animate({opacity: 1}, 500);
-
-
-    //recupere la taille
-    var w = (document.getElementById(divID).offsetWidth)/2;
-    var h = (document.getElementById(divID).offsetWidth)/2;
-    // recentre
-    document.getElementById(divID).style.margin = "-"+h+"px 0 0 -"+w+"px";
-    
+    // rend transparent le fond
     $('#pageCenter').animate({opacity: 0.5}, 500);
 }
+
+///////////////////////////////////////////////////////
+// Fonction qui centre les pop apres leur apparition //
+///////////////////////////////////////////////////////
+function centerElmt(divID){
+    //si c'est la popOver des news, centre auto avec fonction centerElmt    
+    if(divID != "popOverPicture"){
+	//on le centre
+	document.getElementById(divID).style.left = "50%";
+	document.getElementById(divID).style.top = "50%";
+	//recupere la taille
+	var w = (document.getElementById(divID).offsetWidth)/2;
+	var h = (document.getElementById(divID).offsetHeight)/2;
+	// recentre
+	document.getElementById(divID).style.margin = "-"+h+"px 0 0 -"+w+"px";
+    }
+    //sinon c'est la popOver image on centre a la main
+    else{
+	document.getElementById(divID).style.width="95%";
+	document.getElementById(divID).style.height="auto";
+	document.getElementById(divID).style.margin = "0px 0 0 "+2+"%";
+    }
+}
+
 
 /////////////////////////////////////////
 // ferme de maniere animee une popOver //
@@ -43,7 +62,7 @@ function closePopOver(divID) {
 // ajoute le code necessaire à popOverPicture.html pour afficher la photo cliquee //
 ////////////////////////////////////////////////////////////////////////////////////
 function buildPopOverPicture(num){
-    var pop = "<img src='images/gallery/"+num+".jpg' alt='"+num+"'/><br/><br/><a href=\"javascript:closePopOver('popOverPicture');\" class='icon-remove'>FERMER</a>"
+    var pop = "<img src='images/gallery/HD/"+num+".jpg' alt='"+num+"'/><br/><br/><a href=\"javascript:closePopOver('popOverPicture');\" class='icon-remove'>FERMER</a>"
     document.getElementById('popOverPicture').innerHTML = pop;
     document.getElementsByName("popOverPictureOpen")[0].value = true;
     showPopOver('popOverPicture');
